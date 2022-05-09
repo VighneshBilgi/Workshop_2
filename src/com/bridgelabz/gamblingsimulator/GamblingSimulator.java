047eaddd87bfcf544a8ab9db151bfd42f05eb40f
VighneshBilgi <vhbilgi@gmail.com>
2022-05-09 21:13:54 +0530

    Added: UC1_Startting_Stake

--- src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
@@ -3,5 +3,8 @@ package com.bridgelabz.gamblingsimulator;
 public class GamblingSimulator {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator");
+
+        int stake = 100;
+        int bet = 1;
     }
 }
