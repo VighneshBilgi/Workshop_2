61701f919e507f9890f91cde58f30afb7ef141a5
VighneshBilgi <vhbilgi@gmail.com>
2022-05-09 19:56:29 +0530

    Added: Welcome to the Gambling Simulator

---
+++ src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.gamblingsimulator;
+
+public class GamblingSimulator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambling Simulator");
+    }
+}
