e678fd9008e8f7fd2727326a19c70a0aee14e8b8
VighneshBilgi <vhbilgi@gmail.com>
2022-05-10 18:31:06 +0530

    Added: UC2_Win_or_Lose

--- src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
@@ -6,5 +6,16 @@ public class GamblingSimulator {
 
         int stake = 100;
         int bet = 1;
+
+        double chance = Math.random();
+        if(chance <0.5){
+            System.out.println("You lose 1 dollar :(");
+            stake = stake - bet;
+        }else{
+            System.out.println("You won 1 dollar :)");
+            stake = stake + bet;
+        }
+        System.out.println("Your stake currently is :"+stake);
+
     }
 }
